db849920a6206c317bd9fdaf1e844e4560b63eee
mateuszen44-droid <mateuszen44@gmail.com>
2026-02-25 10:17:18 -0300

    Atualizar o sw.js

--- sw.js
+++ sw.js
@@ -1,24 +1,34 @@
-const CACHE = 'app-cristao-v1';
-const ASSETS = ['./','./index.html','./manifest.webmanifest'];
+const CACHE = "devocional-30dias-v1";
+const CORE = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./icon-192.png",
+  "./icon-512.png"
+];
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(k => k !== CACHE ? caches.delete(k) : null))
-    )
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(CORE)).then(() => self.skipWaiting())
   );
-  self.clients.claim();
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then(res =>
-      res || fetch(e.request).catch(() => caches.match('./index.html'))
-    )
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.map(k => (k !== CACHE ? caches.delete(k) : Promise.resolve()))
+    )).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return cached || fetch(event.request).then((resp) => {
+        const copy = resp.clone();
+        caches.open(CACHE).then((cache) => cache.put(event.request, copy)).catch(()=>{});
+        return resp;
+      }).catch(()=>cached);
+    })
   );
 });
